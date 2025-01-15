6925433d80a9bf7d37a54c2f4ba87c3c5826af12
IDGS-805-22000973 <cerveraburrolaj@gmail.com>
2025-01-14 19:19:52 -0600

    Primer commit

---
+++ tema3_ifelse.py
@@ -0,0 +1,14 @@
+num1=3
+num2=8
+"""
+relacionales
+aritmeticos
+booleanos
+"""
+if num1!=num2:
+    if num1>num2:
+        print("El valor {} es mayor {}".format(num1,num2))
+    else:
+        print("El valor {} es menor que {}".format(num1,num2))
+else:
+    pass
---
+++ tema6_funciones.py
@@ -0,0 +1,13 @@
+def funcion1():
+    print ("Hola soy la funcion 1")
+
+def funcion2():
+    print("Hola sou funcion 2")
+
+
+op = int(input("Numero: "))
+
+if op ==1:
+    funcion1()
+else:
+    funcion2()
