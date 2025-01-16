aa99404d6b21782fc2c126ebc2772288b9cc8489
IDGS-805-22000973 <cerveraburrolaj@gmail.com>
2025-01-15 23:18:26 -0600

    commit del 14/Enero/2025

---
+++ tema1_variables.py
@@ -0,0 +1,10 @@
+num1=2
+num2=3.5
+nombre="Mario"
+print(num1)
+print(num2)
+n1="7"
+n2="8"
+
+
+suma = int(n1)+int(2)
--- tema6_funciones.py
+++ tema6_funciones.py
@@ -1,13 +1,34 @@
+import os
 def funcion1():
-    print ("Hola soy la funcion 1")
+    os.system('cls')
+    print ("Dame dos numeros")
+    num1 = int (input("Primer numero: "))
+    num2 = int (input("Segundo numero: "))
+    res=num1+num2
+    print("La suma de {} y {} es {}".format(num1,num2,res))
+    
 
 def funcion2():
     print("Hola sou funcion 2")
 
 
-op = int(input("Numero: "))
+def run():
+    os.system('cls')
+    print("Menu de operaciones")
+    print("1 Suma de operaciones")
+    print("2 otra opcion")
+    print("Salir")
+    opcion = int(input("Ingresa una opcion"))
+    if opcion == 1:
+        funcion1()
+    elif opcion == 2:
+        funcion2()
+    
 
-if op ==1:
-    funcion1()
-else:
-    funcion2()
+
+if __name__ == '__main__':
+    run()
+
+    
+
+
